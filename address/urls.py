6367aba07372173e0898eb5a5848f66488dc8aa0
Shonnetta Johnson-Perez <Skjperez@Shonnettas-MBP.fios-router.home>
2021-05-08 18:27:22 -0400

    added registration page

--- address/urls.py
+++ address/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('address/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('addressapp/', include('addressapp.urls')),
     
 ]
